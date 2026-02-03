93deb75e607555662c304dca0a267a68f7a09463
edigonzales <edi.gonzales@gmail.com>
2026-02-03 11:18:38 +0100

    initial commit

---
+++ src/main/java/ch/so/agi/sodata/SodataApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.sodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SodataApplication.class, args);
+    }
+
+}
